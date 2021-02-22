9c96ede33cb73b339e17a0ff93f204d63908d34b
Kristina <xkris777@gmail.com>
2021-02-22 18:53:01 +0100

    J- function

---
+++ J-functions/exercise.js
@@ -0,0 +1,12 @@
+function halve(number) {
+  // complete the function here
+
+
+return number/2 ;
+
+}
+
+
+let result = halve(12);
+
+console.log(result);
---
+++ J-functions/exercise2.js
@@ -0,0 +1,19 @@
+function triple(number) {
+  // complete function here
+
+  return number *3
+}
+
+
+function divide(number) {
+  
+return number /5
+
+
+}
+
+
+const result = triple(12);
+const resultDivide = divide(2345)
+console.log(result);
+console.log(resultDivide);
