322743c4b7c748ef68633bbeeb6fd86002cfb7f7
Kristina <xkris777@gmail.com>
2021-02-22 23:41:38 +0100

    L-function-nested

---
+++ L-functions-nested/exercise.js
@@ -0,0 +1,56 @@
+//Exercise 1
+
+const PercentageStudents = 650;
+const PercentageMentors = 350;
+
+const total = 1000;
+
+percent1 = PercentageStudents*100/total;
+percent2 = PercentageMentors*100/total;
+
+const structure1 = "Percentage students:";
+const structure2 = "Percentage mentors:";
+
+const mes1 = structure1;
+const mes2 = structure2;
+
+
+console.log(mes1 + percent1 + "%");
+console.log(mes2 + percent2 + "%");
+
+     //console.log( Math.round ${PercentageStudents} + ${PercentageMentors});
+
+
+// Percentage students: 65%
+// Percentage mentors: 35%
+
+
+
+
+// Exercise 2
+
+const mentor1 = "Daniel";
+const mentor2 = "Irina";
+const mentor3 = "Mimi";
+const mentor4 = "Rob";
+const mentor5 = "Yohannes";
+
+
+const greeting = "HELLO";
+
+console.log(greeting + " " + mentor1.toUpperCase());
+console.log(greeting + " " + mentor2.toUpperCase());
+console.log(greeting + " " + mentor3.toUpperCase());
+console.log(greeting + " " + mentor4.toUpperCase());
+console.log(greeting + " " + mentor5.toUpperCase());
+
+
+/*
+HELLO DANIEL
+HELLO IRINA
+HELLO MIMI
+HELLO ROB
+HELLO YOHANNES
+*/
+
+
