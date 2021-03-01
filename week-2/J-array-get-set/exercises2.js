a21e18a0710a69bb251327dd76f85b30408a1ee4
Kristina <xkris777@gmail.com>
2021-03-01 21:02:50 +0100

    new J

---
+++ week-2/J-array-get-set/exercises2.js
@@ -0,0 +1,29 @@
+/*
+  Array setters
+  -------------
+  WITHOUT changing the array literal declaration,
+  - assign the number 4 to the end of this array
+  - change the first value in the array to the number 1
+*/function first(arr) {
+
+  return arr[0];
+}
+
+function last(arr) {
+
+  return arr[0];
+}
+
+
+var numbers = [1, 2, 3]; // Don't change this array literal declaration
+var names = ("Irina", "Ashleigh", "Mozafa");
+/* 
+  DO NOT EDIT BELOW THIS LINE
+  --------------------------- */
+console.log(first(numbers));
+console.log(last(names));
+/* 
+  EXPECTED RESULT
+  ---------------
+  [1, 2, 3, 4]
+*/
